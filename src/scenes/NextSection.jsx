cc7d785d56927e98d91a371e045dffa34594060b
BeHFaR <behfar.behzad@gmail.com>
2026-07-17 15:36:53 +0200

    feat(scenes): add next section placeholder

---
+++ src/scenes/NextSection.jsx
@@ -0,0 +1,9 @@
+import styles from './NextSection.module.css';
+
+export default function NextSection() {
+  return (
+    <section className={styles.section}>
+      <h2 className={styles.heading}>More to come</h2>
+    </section>
+  );
+}
