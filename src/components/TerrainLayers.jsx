26b40c9059ff2e49a11a9a4bbfdecb6fa3883a47
BeHFaR <behfar.behzad@gmail.com>
2026-08-05 13:50:35 +0200

    chore(TerrainLayers): replace the png with a lighter figure

--- src/components/TerrainLayers.jsx
+++ src/components/TerrainLayers.jsx
@@ -10,11 +10,15 @@ export default function TerrainLayers() {
       <img src="/svg/terrain-2-trees.svg" alt="" className={`${styles.layer} ${styles.trees}`} />
       <img src="/svg/terrain-3-mid.svg" alt="" className={`${styles.layer} ${styles.mid}`} />
       <img
-        src="/png/man_with_telescope.png"
+        src="/png/man_with_telescope_light.png"
         alt="Silhouette of a person looking through a telescope at the night sky"
         className={styles.figure}
       />
-      <img src="/svg/terrain-4-fore.svg" alt="" className={`${styles.layer} ${styles.foreground}`} />
+      <img
+        src="/svg/terrain-4-fore.svg"
+        alt=""
+        className={`${styles.layer} ${styles.foreground}`}
+      />
     </>
   );
 }
